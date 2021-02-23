debc522951bbe93d7a87b3ac88fe706958033f5f
Corrine Wheeler <36771713+wheelercorrine@users.noreply.github.com>
2021-02-22 19:40:30 -0500

    Create React App and basic solutiom

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import customData from './nested.json';
+import './App.css';
+
+function App() {
+  var data = customData;
+
+  return (
+    <div className="App">
+      <Grid data={data}/>
+    </div>
+  );
+}
+
+function Grid(props){
+
+  var data = props.data
+  var children = props.data.children;
+
+  var gridStyle={
+    display: "grid",
+    gridTemplateColumns: `repeat(${data.gridSize.width},1fr)`,
+    gridTemplateRows: `repeat(${data.gridSize.height},1fr)`,
+    backgroundColor: data.backgroundColor,
+    height: "100%",
+    width: "100%"
+  };
+
+  if(data.size){
+    gridStyle={
+      ...gridStyle,
+      gridColumnStart: data.position.x + 1,
+      gridColumnEnd: data.position.x + data.size.width + 1,
+      gridRowStart: data.position.y + 1,
+      gridRowEnd: data.position.y + data.size.height + 1
+    }
+  }
+
+  return(
+      <div style={gridStyle}>
+        {children?
+          children.map(function(child){
+            if(child.type === "grid"){
+              return <Grid data={child}/>
+            } else{
+              return <Block data={child}/>
+            }
+          }) : null
+        }
+      </div>
+  )
+
+}
+
+function Block(props){
+  const data=props.data;
+  const blockStyle={
+    backgroundColor: data.color,
+    gridColumnStart: data.position.x + 1,
+    gridColumnEnd: data.position.x + data.size.width + 1,
+    gridRowStart: data.position.y + 1,
+    gridRowEnd: data.position.y + data.size.height + 1
+  }
+  return(
+    <div style={blockStyle}></div>
+  )
+}
+
+export default App;
